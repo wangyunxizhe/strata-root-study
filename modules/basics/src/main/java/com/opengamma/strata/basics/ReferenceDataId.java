9ca5282e148edc449bcab0d2e4f62e0d2d0cec10
wangyunxizhe <543456229@qq.com>
2019-05-24 17:14:48 +0800

    关于ReferenceDataId（参考数据id）的注释

--- modules/basics/src/main/java/com/opengamma/strata/basics/ReferenceDataId.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/ReferenceDataId.java
@@ -6,33 +6,32 @@
 package com.opengamma.strata.basics;
 
 /**
- * An identifier for a unique item of reference data.
+ * 唯一引用数据的标识符。
  * <p>
- * Reference data is obtained from an instance of {@link ReferenceData} using this identifier.
+ * 使用此标识符从{@link ReferenceData} 的实例中获取引用数据。
  *
- * @param <T>  the type of the reference data this identifier refers to
+ * @param <T> 参考数据的数据类型
  */
 public interface ReferenceDataId<T> {
 
-  /**
-   * Gets the type of data this identifier refers to.
-   *
-   * @return the type of the reference data this identifier refers to
-   */
-  public abstract Class<T> getReferenceDataType();
+    /**
+     * 获取参考数据的类型。
+     *
+     * @return 参考数据的数据类型
+     */
+    public abstract Class<T> getReferenceDataType();
 
-  /**
-   * Low-level method to query the reference data value associated with this identifier,
-   * returning null if not found.
-   * <p>
-   * This is a low-level method that obtains the reference data value, returning null instead of an error.
-   * Applications should use {@link ReferenceData#getValue(ReferenceDataId)} in preference to this method.
-   *
-   * @param refData  the reference data to lookup the value in
-   * @return the reference data value, null if not found
-   */
-  public default T queryValueOrNull(ReferenceData refData) {
-    return refData.queryValueOrNull(this);
-  }
+    /**
+     * 用于查询与此标识符关联的引用数据值的低级方法，如果未找到，则返回空值。
+     * <p>
+     * 这是一个获取引用数据值的低级方法，返回空值而不是错误。
+     * 应用程序应优先使用{@link ReferenceData#getValue(ReferenceDataId)} 而不是此方法。
+     *
+     * @param refData 参考数据
+     * @return 未找到数据返回null
+     */
+    public default T queryValueOrNull(ReferenceData refData) {
+        return refData.queryValueOrNull(this);
+    }
 
 }
