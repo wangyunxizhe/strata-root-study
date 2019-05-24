2ddd0c0fee9924e2311b5f9f1970de8654bf0826
wangyunxizhe <543456229@qq.com>
2019-05-24 16:52:22 +0800

    利用静态代码块在该类被调用时完成对参考数据的组装

--- modules/basics/src/main/java/com/opengamma/strata/basics/StandardReferenceData.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/StandardReferenceData.java
@@ -5,44 +5,46 @@
  */
 package com.opengamma.strata.basics;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.google.common.collect.ImmutableMap;
 import com.opengamma.strata.basics.date.HolidayCalendar;
 import com.opengamma.strata.basics.date.HolidayCalendars;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Provides standard reference data for holiday calendars in common currencies.
+ * 为常用货币的HolidayCalendar提供标准参考数据。
  */
 final class StandardReferenceData {
 
-  /**
-   * Standard reference data.
-   */
-  static final ImmutableReferenceData STANDARD;
-  static {
-    Map<ReferenceDataId<?>, Object> map = new HashMap<>();
-    for (HolidayCalendar cal : HolidayCalendars.extendedEnum().lookupAllNormalized().values()) {
-      map.put(cal.getId(), cal);
-    }
-    STANDARD = ImmutableReferenceData.of(map);
-  }
-  /**
-   * Minimal reference data.
-   */
-  static final ImmutableReferenceData MINIMAL;
-  static {
-    ImmutableMap.Builder<ReferenceDataId<?>, Object> builder = ImmutableMap.builder();
-    builder.put(HolidayCalendars.NO_HOLIDAYS.getId(), HolidayCalendars.NO_HOLIDAYS);
-    builder.put(HolidayCalendars.SAT_SUN.getId(), HolidayCalendars.SAT_SUN);
-    builder.put(HolidayCalendars.FRI_SAT.getId(), HolidayCalendars.FRI_SAT);
-    builder.put(HolidayCalendars.THU_FRI.getId(), HolidayCalendars.THU_FRI);
-    MINIMAL = ImmutableReferenceData.of(builder.build());
-  }
+    /**
+     * 标准参考数据
+     */
+    static final ImmutableReferenceData STANDARD;
 
-  // restricted constructor
-  private StandardReferenceData() {
-  }
+    static {
+        Map<ReferenceDataId<?>, Object> map = new HashMap<>();
+        for (HolidayCalendar cal : HolidayCalendars.extendedEnum().lookupAllNormalized().values()) {
+            map.put(cal.getId(), cal);
+        }
+        STANDARD = ImmutableReferenceData.of(map);
+    }
+
+    /**
+     * 最小参考数据。
+     */
+    static final ImmutableReferenceData MINIMAL;
+
+    static {
+        ImmutableMap.Builder<ReferenceDataId<?>, Object> builder = ImmutableMap.builder();
+        builder.put(HolidayCalendars.NO_HOLIDAYS.getId(), HolidayCalendars.NO_HOLIDAYS);
+        builder.put(HolidayCalendars.SAT_SUN.getId(), HolidayCalendars.SAT_SUN);
+        builder.put(HolidayCalendars.FRI_SAT.getId(), HolidayCalendars.FRI_SAT);
+        builder.put(HolidayCalendars.THU_FRI.getId(), HolidayCalendars.THU_FRI);
+        MINIMAL = ImmutableReferenceData.of(builder.build());
+    }
+
+    private StandardReferenceData() {
+    }
 
 }
