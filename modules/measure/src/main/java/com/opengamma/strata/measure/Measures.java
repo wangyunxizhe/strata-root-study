524f72dce2509e7b4001cdca027c71b5e91c8ea2
wangyunxizhe <543456229@qq.com>
2019-05-26 15:47:43 +0800

    Strata计算用的标准度量集合，都是跟金融相关的计量单位，详见类中注解

--- modules/measure/src/main/java/com/opengamma/strata/measure/Measures.java
+++ modules/measure/src/main/java/com/opengamma/strata/measure/Measures.java
@@ -10,135 +10,127 @@ import com.opengamma.strata.calc.Measure;
 import com.opengamma.strata.data.scenario.ScenarioArray;
 
 /**
- * The standard set of measures that can be calculated by Strata.
+ * Strata计算用的标准度量集合，都是跟金融相关的计量单位。
+ * 总结：就是想计算什么就在这个类中找
+ * 对应的配置文件位置：
+ * modules\measure\src\main\resources\META-INF\com\opengamma\strata\config\base2\Measure.ini
  * <p>
- * A measure identifies the calculation result that is required.
- * For example present value, par rate or spread.
+ * 度量值标识所需的计算结果。例如现值、票面利率或价差。
  * <p>
- * Note that not all measures will be available for all targets.
+ * 请注意，并非所有Measures对象都适用于所有目标。
  */
 public final class Measures {
 
-  /**
-   * Measure representing the present value of the calculation target.
-   * <p>
-   * The result is a single currency monetary amount in the reporting currency.
-   */
-  public static final Measure PRESENT_VALUE = Measure.of(StandardMeasures.PRESENT_VALUE.getName());
-  /**
-   * Measure representing a break-down of the present value calculation on the target.
-   * <p>
-   * No currency conversion is performed on the monetary amounts.
-   */
-  public static final Measure EXPLAIN_PRESENT_VALUE = Measure.of(StandardMeasures.EXPLAIN_PRESENT_VALUE.getName());
+    /**
+     * 表示计算目标的现值的度量值。
+     * <p>
+     * 结果是以报告货币表示的单一货币金额。
+     */
+    public static final Measure PRESENT_VALUE = Measure.of(StandardMeasures.PRESENT_VALUE.getName());
+    /**
+     * 表示目标现值计算细分的度量。
+     * <p>
+     * 货币金额不进行货币转换。
+     */
+    public static final Measure EXPLAIN_PRESENT_VALUE = Measure.of(StandardMeasures.EXPLAIN_PRESENT_VALUE.getName());
 
-  //-------------------------------------------------------------------------
-  /**
-   * Measure representing the calibrated sum PV01 on the calculation target.
-   * <p>
-   * This is the sensitivity of present value to a one basis point shift in the calibrated data structure.
-   * The result is the sum of the sensitivities of all affected curves.
-   * It is expressed in the reporting currency.
-   */
-  public static final Measure PV01_CALIBRATED_SUM = Measure.of(StandardMeasures.PV01_CALIBRATED_SUM.getName());
-  /**
-   * Measure representing the calibrated bucketed PV01 on the calculation target.
-   * <p>
-   * This is the sensitivity of present value to a one basis point shift in the calibrated data structure.
-   * The result is provided for each affected curve and currency, bucketed by parameter.
-   * It is expressed in the reporting currency.
-   */
-  public static final Measure PV01_CALIBRATED_BUCKETED = Measure.of(StandardMeasures.PV01_CALIBRATED_BUCKETED.getName());
-  /**
-   * Measure representing the market quote sum PV01 on the calculation target.
-   * <p>
-   * This is the sensitivity of present value to a one basis point shift in the
-   * market quotes used to calibrate the data structure.
-   * The result is the sum of the sensitivities of all affected curves.
-   * It is expressed in the reporting currency.
-   */
-  public static final Measure PV01_MARKET_QUOTE_SUM = Measure.of(StandardMeasures.PV01_MARKET_QUOTE_SUM.getName());
-  /**
-   * Measure representing the market quote bucketed PV01 on the calculation target.
-   * <p>
-   * This is the sensitivity of present value to a one basis point shift in the
-   * market quotes used to calibrate the data structure.
-   * The result is provided for each affected curve and currency, bucketed by parameter.
-   * It is expressed in the reporting currency.
-   */
-  public static final Measure PV01_MARKET_QUOTE_BUCKETED = Measure.of(StandardMeasures.PV01_MARKET_QUOTE_BUCKETED.getName());
+    //-------------------------------------------------------------------------
+    /**
+     * 在计算目标上表示校准和PV01的测量值。
+     * <p>
+     * 这是当前值对校准数据结构中一个基点位移的敏感性。结果是所有受影响曲线的灵敏度之和。以报告货币表示。
+     */
+    public static final Measure PV01_CALIBRATED_SUM = Measure.of(StandardMeasures.PV01_CALIBRATED_SUM.getName());
+    /**
+     * 测量在计算目标上表示校准的桶形PV01。
+     * <p>
+     * 这是当前值对校准数据结构中一个基点位移的敏感性。
+     * 结果被提供给每个受影响的曲线和货币，由参数来表示。它用报告货币表示。
+     */
+    public static final Measure PV01_CALIBRATED_BUCKETED = Measure.of(StandardMeasures.PV01_CALIBRATED_BUCKETED.getName());
+    /**
+     * 在计算目标上表示市场报价总和PV01。
+     * <p>
+     * 这是现值对用于校准数据结构的市场报价中一个基点变化的敏感性。
+     * 其结果是所有受影响的曲线的灵敏度之和。它用报告货币表示。
+     */
+    public static final Measure PV01_MARKET_QUOTE_SUM = Measure.of(StandardMeasures.PV01_MARKET_QUOTE_SUM.getName());
+    /**
+     * 在计算目标上代表市场报价PV01的度量。
+     * <p>
+     * 这是现值对用于校准数据结构的市场报价中一个基点变化的敏感性。
+     * 结果被提供给每个受影响的曲线和货币，由参数来表示。它用报告货币表示。
+     */
+    public static final Measure PV01_MARKET_QUOTE_BUCKETED = Measure.of(StandardMeasures.PV01_MARKET_QUOTE_BUCKETED.getName());
 
-  //-------------------------------------------------------------------------
-  /**
-   * Measure representing the par rate of the calculation target.
-   */
-  public static final Measure PAR_RATE = Measure.of(StandardMeasures.PAR_RATE.getName());
-  /**
-   * Measure representing the par spread of the calculation target.
-   */
-  public static final Measure PAR_SPREAD = Measure.of(StandardMeasures.PAR_SPREAD.getName());
+    //-------------------------------------------------------------------------
+    /**
+     * 表示计算目标的PAR率的度量。
+     */
+    public static final Measure PAR_RATE = Measure.of(StandardMeasures.PAR_RATE.getName());
+    /**
+     * 表示计算目标的PAR扩展的度量。
+     */
+    public static final Measure PAR_SPREAD = Measure.of(StandardMeasures.PAR_SPREAD.getName());
 
-  //-------------------------------------------------------------------------
-  /**
-   * Measure representing the present value of each leg of the calculation target.
-   * <p>
-   * The result is expressed in the reporting currency.
-   */
-  public static final Measure LEG_PRESENT_VALUE = Measure.of(StandardMeasures.LEG_PRESENT_VALUE.getName());
-  /**
-   * Measure representing the initial notional amount of each leg of the calculation target.
-   * <p>
-   * The result is expressed in the reporting currency.
-   */
-  public static final Measure LEG_INITIAL_NOTIONAL = Measure.of(StandardMeasures.LEG_INITIAL_NOTIONAL.getName());
-  /**
-   * Measure representing the accrued interest of the calculation target.
-   */
-  public static final Measure ACCRUED_INTEREST = Measure.of(StandardMeasures.ACCRUED_INTEREST.getName());
-  /**
-   * Measure representing the cash flows of the calculation target.
-   * <p>
-   * Cash flows provide details about the payments of the target.
-   * The result is expressed in the reporting currency.
-   */
-  public static final Measure CASH_FLOWS = Measure.of(StandardMeasures.CASH_FLOWS.getName());
-  /**
-   * Measure representing the currency exposure of the calculation target.
-   * <p>
-   * Currency exposure is the currency risk, expressed as the equivalent amount in each currency.
-   * Calculated values are not converted to the reporting currency and may contain values in multiple currencies
-   * if the target contains multiple currencies.
-   */
-  public static final Measure CURRENCY_EXPOSURE = Measure.of(StandardMeasures.CURRENCY_EXPOSURE.getName());
-  /**
-   * Measure representing the current cash of the calculation target.
-   * <p>
-   * Current cash is the sum of all cash flows paid on the valuation date.
-   * The result is expressed in the reporting currency.
-   */
-  public static final Measure CURRENT_CASH = Measure.of(StandardMeasures.CURRENT_CASH.getName());
-  /**
-   * Measure representing the forward FX rate of the calculation target.
-   */
-  public static final Measure FORWARD_FX_RATE = Measure.of(StandardMeasures.FORWARD_FX_RATE.getName());
-  /**
-   * Measure representing the unit price of the instrument.
-   * <p>
-   * This is the price of a single unit of a security using Strata market conventions.
-   * The price is represented as a {@code double}, even if it is actually a currency amount.
-   */
-  public static final Measure UNIT_PRICE = Measure.of(StandardMeasures.UNIT_PRICE.getName());
-  /**
-   * Measure representing the resolved form of the calculation target.
-   * <p>
-   * Many calculation targets have a {@linkplain Resolvable resolved} form that is optimized for pricing.
-   * This measure allows the resolved form to be obtained.
-   * Since the target is the same for all scenarios, the result is not wrapped in {@link ScenarioArray}.
-   */
-  public static final Measure RESOLVED_TARGET = Measure.of(StandardMeasures.RESOLVED_TARGET.getName());
+    //-------------------------------------------------------------------------
+    /**
+     * 表示计算目标的每一条分支的当前值的度量。
+     * <p>
+     * 结果以报告货币表示。
+     */
+    public static final Measure LEG_PRESENT_VALUE = Measure.of(StandardMeasures.LEG_PRESENT_VALUE.getName());
+    /**
+     * 表示计算目标每一条分支的初始名义金额的度量。
+     * <p>
+     * 结果以报告货币表示。
+     */
+    public static final Measure LEG_INITIAL_NOTIONAL = Measure.of(StandardMeasures.LEG_INITIAL_NOTIONAL.getName());
+    /**
+     * 表示计算目标的应计利息的度量值。
+     */
+    public static final Measure ACCRUED_INTEREST = Measure.of(StandardMeasures.ACCRUED_INTEREST.getName());
+    /**
+     * 衡量计算目标的现金流量。
+     * <p>
+     * 现金流提供目标公司付款的详细信息。结果以报告货币表示。
+     */
+    public static final Measure CASH_FLOWS = Measure.of(StandardMeasures.CASH_FLOWS.getName());
+    /**
+     * 表示计算目标的货币风险的度量。
+     * <p>
+     * 货币风险是货币风险，以每种货币的等值金额表示。
+     * 计算值不转换为报告货币，如果目标包含多种货币，则可能包含多种货币的值。
+     */
+    public static final Measure CURRENCY_EXPOSURE = Measure.of(StandardMeasures.CURRENCY_EXPOSURE.getName());
+    /**
+     * 表示计算目标当前现金的度量。
+     * <p>
+     * 当前现金是在估价日支付的所有现金流的总和。结果以报告货币表示。
+     */
+    public static final Measure CURRENT_CASH = Measure.of(StandardMeasures.CURRENT_CASH.getName());
+    /**
+     * 表示计算目标的正向FX速率的度量。
+     */
+    public static final Measure FORWARD_FX_RATE = Measure.of(StandardMeasures.FORWARD_FX_RATE.getName());
+    /**
+     * 表示仪器单价的计量单位。
+     * <p>
+     * 这是使用分层市场约定的单个证券单位的价格。
+     * 价格表示为{@code double}，即使它实际上是一个货币金额。
+     */
+    public static final Measure UNIT_PRICE = Measure.of(StandardMeasures.UNIT_PRICE.getName());
+    /**
+     * 表示计算目标的解析形式的度量。
+     * <p>
+     * 许多计算目标有一个优化的{@linkplain Resolvable resolved}形式，用于定价。
+     * This measure allows the resolved form to be obtained.
+     * 由于目标对于所有场景都是相同的，因此结果不包含在{@link ScenarioArray}中。
+     */
+    public static final Measure RESOLVED_TARGET = Measure.of(StandardMeasures.RESOLVED_TARGET.getName());
 
-  //-------------------------------------------------------------------------
-  private Measures() {
-  }
+    //-------------------------------------------------------------------------
+    private Measures() {
+    }
 
 }
