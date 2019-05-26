6ef6e9adf869c6a57fc9cf569f800ebf6989d294
wangyunxizhe <543456229@qq.com>
2019-05-26 14:30:55 +0800

    各个ProductTrade接口继承的父类接口，包含TradeInfo内中的各个属性

--- modules/product/src/main/java/com/opengamma/strata/product/Trade.java
+++ modules/product/src/main/java/com/opengamma/strata/product/Trade.java
@@ -8,41 +8,40 @@ package com.opengamma.strata.product;
 import com.opengamma.strata.product.common.SummarizerUtils;
 
 /**
- * A trade with additional structured information.
+ * 附加结构化信息的交易。
  * <p>
- * A trade is a transaction that occurred on a specific date between two counterparties.
- * For example, an interest rate swap trade agreed on a particular date for
- * cash-flows in the future.
+ * Trade是在两个交易对手之间的特定日期发生的交易。例如，在未来现金流的特定日期商定的利率互换交易。
  * <p>
- * The reference to {@link TradeInfo} captures structured information common to different types of trade.
+ * 对{@link TradeInfo}的引用捕获了不同类型交易常见的结构化信息。
  * <p>
- * Implementations of this interface must be immutable beans.
+ * 此接口的实现必须是不可变的bean。
  */
 public interface Trade
-    extends PortfolioItem {
+        extends PortfolioItem {
 
-  @Override
-  public default PortfolioItemSummary summarize() {
-    return SummarizerUtils.summary(this, ProductType.OTHER, "Unknown: " + getClass().getSimpleName());
-  }
+    @Override
+    public default PortfolioItemSummary summarize() {
+        return SummarizerUtils.summary(this, ProductType.OTHER, "Unknown: " + getClass().getSimpleName());
+    }
 
-  /**
-   * Gets the standard trade information.
-   * <p>
-   * All trades contain this standard set of information.
-   * 
-   * @return the trade information
-   */
-  @Override
-  public abstract TradeInfo getInfo();
+    /**
+     * 获取标准交易信息。
+     * <p>
+     * 所有交易都包含这套标准信息。
+     *
+     * @return the trade information
+     */
+    @Override
+    public abstract TradeInfo getInfo();
 
-  //-------------------------------------------------------------------------
-  /**
-   * Returns an instance with the specified info.
-   * 
-   * @param info  the new info
-   * @return the instance with the specified info
-   */
-  public abstract Trade withInfo(TradeInfo info);
+    //-------------------------------------------------------------------------
+
+    /**
+     * 根据传入的TradeInfo返回具有指定Trade实例。
+     *
+     * @param info the new info
+     * @return the instance with the specified info
+     */
+    public abstract Trade withInfo(TradeInfo info);
 
 }
