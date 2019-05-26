bde5bb16ee128e928b1522d37ecee0368db4940a
wangyunxizhe <543456229@qq.com>
2019-05-26 09:55:05 +0800

    Product相关类的顶层接口，其中是对获取Currency的不可变集合的方法抽象

--- modules/product/src/main/java/com/opengamma/strata/product/Product.java
+++ modules/product/src/main/java/com/opengamma/strata/product/Product.java
@@ -9,53 +9,51 @@ import com.google.common.collect.ImmutableSet;
 import com.opengamma.strata.basics.currency.Currency;
 
 /**
- * The product details of a financial instrument.
+ * 金融工具的产品细节。
  * <p>
- * A product is a high level abstraction applicable to many different types.
- * For example, an Interest Rate Swap is a product, as is a Forward Rate Agreement (FRA).
+ * Product接口是适用于许多不同类型的高级抽象。
+ * 例如，利率互换（Swap）是一种产品，远期利率协议（FRA）也是如此。
+ * 用代码说就是该接口是Swap，也是Dsf的超类
  * <p>
- * A product exists independently from a {@link Trade}. It represents the economics of the
- * financial instrument regardless of the trade date or counterparties.
+ * product相关的类独立于{@link Trade}存在。
+ * 它代表金融工具的经济性，而不考虑交易日期或交易对手（Trade相关类中的属性）。
  * <p>
- * Implementations must be immutable and thread-safe beans.
+ * 实现必须是不可变的和线程安全的bean。
  */
 public interface Product {
 
-  /**
-   * Checks if this product is cross-currency.
-   * <p>
-   * A cross currency product is defined as one that refers to two or more currencies.
-   * Any product with direct or indirect FX exposure will be cross-currency.
-   * <p>
-   * For example, a fixed/Ibor swap in USD observing USD-LIBOR is not cross currency,
-   * but one that observes EURIBOR is cross currency.
-   * 
-   * @return true if cross currency
-   */
-  public default boolean isCrossCurrency() {
-    return allCurrencies().size() > 1;
-  }
+    /**
+     * 检查此产品是否为交叉货币。
+     * <p>
+     * 跨货币产品定义为指两种或两种以上货币的产品。任何有直接或间接外汇风险的产品都将是交叉货币。
+     * <p>
+     * 例如，fixed/Ibor swap in USD observing USD-LIBOR不是交叉货币，
+     * 但observes EURIBOR的swap是交叉货币。
+     *
+     * @return 是交叉货币，返回true
+     */
+    public default boolean isCrossCurrency() {
+        return allCurrencies().size() > 1;
+    }
 
-  /**
-   * Returns the set of currencies that the product pays in.
-   * <p>
-   * This returns the complete set of payment currencies.
-   * This will typically return one or two currencies.
-   * 
-   * @return the set of payment currencies
-   */
-  public default ImmutableSet<Currency> allPaymentCurrencies() {
-    return allCurrencies();
-  }
+    /**
+     * 返回产品支付的货币集。
+     * <p>
+     * 这将返回一整套付款货币（Currency集合）。这通常会返回一种或两种货币。
+     *
+     * @return the set of payment currencies
+     */
+    public default ImmutableSet<Currency> allPaymentCurrencies() {
+        return allCurrencies();
+    }
 
-  /**
-   * Returns the set of currencies the product refers to.
-   * <p>
-   * This returns the complete set of currencies, not just the payment currencies.
-   * For example, the sets will differ when one of the currencies is non-deliverable.
-   * 
-   * @return the set of currencies the product refers to
-   */
-  public abstract ImmutableSet<Currency> allCurrencies();
+    /**
+     * 返回产品引用的Currency集合。
+     * <p>
+     * 这将返回完整的Currency集合，而不仅仅是支付货币。例如，当其中一种货币不可交付时，这些货币组将有所不同。
+     *
+     * @return the set of currencies the product refers to
+     */
+    public abstract ImmutableSet<Currency> allCurrencies();
 
 }
