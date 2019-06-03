508f393a75e28a45ff959167d29b7e19d4f7b16b
wangyunxizhe <543456229@qq.com>
2019-06-03 17:12:45 +0800

    假期调整为工作日的规则接口，以及定存案列中使用到的该接口的一个常用实现类。定义了如何调整的具体规则

--- modules/basics/src/main/java/com/opengamma/strata/basics/date/BusinessDayConvention.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/date/BusinessDayConvention.java
@@ -5,77 +5,73 @@
  */
 package com.opengamma.strata.basics.date;
 
-import java.time.LocalDate;
-
+import com.opengamma.strata.collect.named.ExtendedEnum;
+import com.opengamma.strata.collect.named.Named;
 import org.joda.convert.FromString;
 import org.joda.convert.ToString;
 
-import com.opengamma.strata.collect.named.ExtendedEnum;
-import com.opengamma.strata.collect.named.Named;
+import java.time.LocalDate;
 
 /**
- * A convention defining how to adjust a date if it falls on a day other than a business day.
+ * 一种规则，定义了当日期落在工作日以外的某一天时如何调整日期。
  * <p>
- * The purpose of this convention is to define how to handle non-business days.
- * When processing dates in finance, it is typically intended that non-business days,
- * such as weekends and holidays, are converted to a nearby valid business day.
- * The convention, in conjunction with a {@linkplain HolidayCalendar holiday calendar},
- * defines exactly how the adjustment should be made.
+ * 本规则的目的是规定如何处理非工作日。在处理财务日期时，通常打算将非工作日（如周末和节假日）转换为附近的有效工作日。
+ * 该规则与{@linkplain HolidayCalendar holiday calendar}结合起来，精确地定义了应如何进行调整。
  * <p>
- * The most common implementations are provided in {@link BusinessDayConventions}.
- * Additional implementations may be added by implementing this interface.
+ * 最常见的实现在{@link BusinessDayConventions}中提供。可通过实现此接口添加其他实现。
  * <p>
- * All implementations of this interface must be immutable and thread-safe.
+ * 此接口的所有实现都必须是不可变的且线程安全的。
  */
 public interface BusinessDayConvention
-    extends Named {
+        extends Named {
 
-  /**
-   * Obtains an instance from the specified unique name.
-   * 
-   * @param uniqueName  the unique name
-   * @return the business convention
-   * @throws IllegalArgumentException if the name is not known
-   */
-  @FromString
-  public static BusinessDayConvention of(String uniqueName) {
-    return extendedEnum().lookup(uniqueName);
-  }
+    /**
+     * Obtains an instance from the specified unique name.
+     *
+     * @param uniqueName the unique name
+     * @return the business convention
+     * @throws IllegalArgumentException if the name is not known
+     */
+    @FromString
+    public static BusinessDayConvention of(String uniqueName) {
+        return extendedEnum().lookup(uniqueName);
+    }
 
-  /**
-   * Gets the extended enum helper.
-   * <p>
-   * This helper allows instances of the convention to be looked up.
-   * It also provides the complete set of available instances.
-   * 
-   * @return the extended enum helper
-   */
-  public static ExtendedEnum<BusinessDayConvention> extendedEnum() {
-    return BusinessDayConventions.ENUM_LOOKUP;
-  }
+    /**
+     * Gets the extended enum helper.
+     * <p>
+     * This helper allows instances of the convention to be looked up.
+     * It also provides the complete set of available instances.
+     *
+     * @return the extended enum helper
+     */
+    public static ExtendedEnum<BusinessDayConvention> extendedEnum() {
+        return BusinessDayConventions.ENUM_LOOKUP;
+    }
 
-  //-------------------------------------------------------------------------
-  /**
-   * Adjusts the date as necessary if it is not a business day.
-   * <p>
-   * If the date is a business day it will be returned unaltered.
-   * If the date is not a business day, the convention will be applied.
-   * 
-   * @param date  the date to adjust
-   * @param calendar  the calendar that defines holidays and business days
-   * @return the adjusted date
-   */
-  public abstract LocalDate adjust(LocalDate date, HolidayCalendar calendar);
+    //-------------------------------------------------------------------------
 
-  /**
-   * Gets the name that uniquely identifies this convention.
-   * <p>
-   * This name is used in serialization and can be parsed using {@link #of(String)}.
-   * 
-   * @return the unique name
-   */
-  @ToString
-  @Override
-  public abstract String getName();
+    /**
+     * 如果不是工作日，则根据需要调整日期。
+     * <p>
+     * 如果日期是工作日，则将原封不动地返回。
+     * 如果该日期不是工作日，则按规则调整。
+     *
+     * @param date     the date to adjust
+     * @param calendar the calendar that defines holidays and business days
+     * @return the adjusted date
+     */
+    public abstract LocalDate adjust(LocalDate date, HolidayCalendar calendar);
+
+    /**
+     * Gets the name that uniquely identifies this convention.
+     * <p>
+     * This name is used in serialization and can be parsed using {@link #of(String)}.
+     *
+     * @return the unique name
+     */
+    @ToString
+    @Override
+    public abstract String getName();
 
 }
--- modules/basics/src/main/java/com/opengamma/strata/basics/date/BusinessDayConventions.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/date/BusinessDayConventions.java
@@ -8,91 +8,90 @@ package com.opengamma.strata.basics.date;
 import com.opengamma.strata.collect.named.ExtendedEnum;
 
 /**
- * Constants and implementations for standard business day conventions.
+ * 标准工作日规则的常量和实现。
  * <p>
- * The purpose of each convention is to define how to handle non-business days.
- * When processing dates in finance, it is typically intended that non-business days,
- * such as weekends and holidays, are converted to a nearby valid business day.
- * The convention, in conjunction with a {@linkplain HolidayCalendar holiday calendar},
- * defines exactly how the adjustment should be made.
+ * 每个规则的目的是定义如何处理非工作日。
+ * 在财务部门处理日期时，通常计划将非工作日（如周末和节假日）转换为附近的有效工作日。
+ * 该公约与{@linkplain HolidayCalendar holiday calendar}结合起来，精确地定义了应如何进行调整。
  */
 public final class BusinessDayConventions {
-  // constants are indirected via ENUM_LOOKUP to allow them to be replaced by config
+    // constants are indirected via ENUM_LOOKUP to allow them to be replaced by config
 
-  /**
-   * The extended enum lookup from name to instance.
-   */
-  static final ExtendedEnum<BusinessDayConvention> ENUM_LOOKUP = ExtendedEnum.of(BusinessDayConvention.class);
+    /**
+     * The extended enum lookup from name to instance.
+     */
+    static final ExtendedEnum<BusinessDayConvention> ENUM_LOOKUP = ExtendedEnum.of(BusinessDayConvention.class);
 
-  /**
-   * The 'NoAdjust' convention which makes no adjustment.
-   * <p>
-   * The input date will not be adjusted even if it is not a business day.
-   */
-  public static final BusinessDayConvention NO_ADJUST =
-      BusinessDayConvention.of(StandardBusinessDayConventions.NO_ADJUST.getName());
-  /**
-   * The 'Following' convention which adjusts to the next business day.
-   * <p>
-   * If the input date is not a business day then the date is adjusted.
-   * The adjusted date is the next business day.
-   */
-  public static final BusinessDayConvention FOLLOWING =
-      BusinessDayConvention.of(StandardBusinessDayConventions.FOLLOWING.getName());
-  /**
-   * The 'ModifiedFollowing' convention which adjusts to the next business day without crossing month end.
-   * <p>
-   * If the input date is not a business day then the date is adjusted.
-   * The adjusted date is the next business day unless that day is in a different
-   * calendar month, in which case the previous business day is returned.
-   */
-  public static final BusinessDayConvention MODIFIED_FOLLOWING =
-      BusinessDayConvention.of(StandardBusinessDayConventions.MODIFIED_FOLLOWING.getName());
-  /**
-   * The 'ModifiedFollowingBiMonthly' convention which adjusts to the next business day without
-   * crossing mid-month or month end.
-   * <p>
-   * If the input date is not a business day then the date is adjusted.
-   * The month is divided into two parts, the first half, the 1st to 15th and the 16th onwards.
-   * The adjusted date is the next business day unless that day is in a different half-month,
-   * in which case the previous business day is returned.
-   */
-  public static final BusinessDayConvention MODIFIED_FOLLOWING_BI_MONTHLY =
-      BusinessDayConvention.of(StandardBusinessDayConventions.MODIFIED_FOLLOWING_BI_MONTHLY.getName());
-  /**
-   * The 'Preceding' convention which adjusts to the previous business day.
-   * <p>
-   * If the input date is not a business day then the date is adjusted.
-   * The adjusted date is the previous business day.
-   */
-  public static final BusinessDayConvention PRECEDING =
-      BusinessDayConvention.of(StandardBusinessDayConventions.PRECEDING.getName());
-  /**
-   * The 'ModifiedPreceding' convention which adjusts to the previous business day without crossing month start.
-   * <p>
-   * If the input date is not a business day then the date is adjusted.
-   * The adjusted date is the previous business day unless that day is in a different
-   * calendar month, in which case the next business day is returned.
-   */
-  public static final BusinessDayConvention MODIFIED_PRECEDING =
-      BusinessDayConvention.of(StandardBusinessDayConventions.MODIFIED_PRECEDING.getName());
-  /**
-   * The 'Nearest' convention which adjusts Sunday and Monday forward, and other days backward.
-   * <p>
-   * If the input date is not a business day then the date is adjusted.
-   * If the input is Sunday or Monday then the next business day is returned.
-   * Otherwise the previous business day is returned.
-   * <p>
-   * Note that despite the name, the algorithm may not return the business day that is actually nearest.
-   */
-  public static final BusinessDayConvention NEAREST =
-      BusinessDayConvention.of(StandardBusinessDayConventions.NEAREST.getName());
+    /**
+     * The 'NoAdjust' convention which makes no adjustment.
+     * <p>
+     * The input date will not be adjusted even if it is not a business day.
+     */
+    public static final BusinessDayConvention NO_ADJUST =
+            BusinessDayConvention.of(StandardBusinessDayConventions.NO_ADJUST.getName());
+    /**
+     * 调整到下一个工作日的“Following”约定。
+     * <p>
+     * 如果输入日期不是工作日，则调整日期。调整日期是下一个工作日。
+     * 底层适用HolidayCalendar.nextOrSame(date)方法实现
+     */
+    public static final BusinessDayConvention FOLLOWING =
+            BusinessDayConvention.of(StandardBusinessDayConventions.FOLLOWING.getName());
+    /**
+     * The 'ModifiedFollowing' convention which adjusts to the next business day without crossing month end.
+     * <p>
+     * If the input date is not a business day then the date is adjusted.
+     * The adjusted date is the next business day unless that day is in a different
+     * calendar month, in which case the previous business day is returned.
+     */
+    public static final BusinessDayConvention MODIFIED_FOLLOWING =
+            BusinessDayConvention.of(StandardBusinessDayConventions.MODIFIED_FOLLOWING.getName());
+    /**
+     * The 'ModifiedFollowingBiMonthly' convention which adjusts to the next business day without
+     * crossing mid-month or month end.
+     * <p>
+     * If the input date is not a business day then the date is adjusted.
+     * The month is divided into two parts, the first half, the 1st to 15th and the 16th onwards.
+     * The adjusted date is the next business day unless that day is in a different half-month,
+     * in which case the previous business day is returned.
+     */
+    public static final BusinessDayConvention MODIFIED_FOLLOWING_BI_MONTHLY =
+            BusinessDayConvention.of(StandardBusinessDayConventions.MODIFIED_FOLLOWING_BI_MONTHLY.getName());
+    /**
+     * The 'Preceding' convention which adjusts to the previous business day.
+     * <p>
+     * If the input date is not a business day then the date is adjusted.
+     * The adjusted date is the previous business day.
+     */
+    public static final BusinessDayConvention PRECEDING =
+            BusinessDayConvention.of(StandardBusinessDayConventions.PRECEDING.getName());
+    /**
+     * The 'ModifiedPreceding' convention which adjusts to the previous business day without crossing month start.
+     * <p>
+     * If the input date is not a business day then the date is adjusted.
+     * The adjusted date is the previous business day unless that day is in a different
+     * calendar month, in which case the next business day is returned.
+     */
+    public static final BusinessDayConvention MODIFIED_PRECEDING =
+            BusinessDayConvention.of(StandardBusinessDayConventions.MODIFIED_PRECEDING.getName());
+    /**
+     * The 'Nearest' convention which adjusts Sunday and Monday forward, and other days backward.
+     * <p>
+     * If the input date is not a business day then the date is adjusted.
+     * If the input is Sunday or Monday then the next business day is returned.
+     * Otherwise the previous business day is returned.
+     * <p>
+     * Note that despite the name, the algorithm may not return the business day that is actually nearest.
+     */
+    public static final BusinessDayConvention NEAREST =
+            BusinessDayConvention.of(StandardBusinessDayConventions.NEAREST.getName());
 
-  //-------------------------------------------------------------------------
-  /**
-   * Restricted constructor.
-   */
-  private BusinessDayConventions() {
-  }
+    //-------------------------------------------------------------------------
+
+    /**
+     * Restricted constructor.
+     */
+    private BusinessDayConventions() {
+    }
 
 }
