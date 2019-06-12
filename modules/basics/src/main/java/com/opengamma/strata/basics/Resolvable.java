482eee4b8917b161e4d752ec575a4ddfe097a7ae
wangyunxizhe <543456229@qq.com>
2019-06-12 14:53:09 +0800

    据引用数据解析对象的接口类，在工程中一般用于对“产品”类进行参考数据的绑定

--- modules/basics/src/main/java/com/opengamma/strata/basics/Resolvable.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/Resolvable.java
@@ -6,43 +6,39 @@
 package com.opengamma.strata.basics;
 
 /**
- * An object that can be resolved against reference data.
+ * 可以根据引用数据解析的对象。
  * <p>
- * Interface marking those objects that can be resolved using {@link ReferenceData}.
- * Implementations of this interface will use {@linkplain ReferenceDataId identifiers}
- * to refer to key concepts, such as holiday calendars and securities.
+ * 接口标记那些可以使用{@link ReferenceData}解析的对象。
+ * 这个接口的实现将使用{@linkplain ReferenceDataId identifiers}来引用关键概念，比如holiday calendars和securities。
  * <p>
- * When the {@code resolve(ReferenceData)} method is called, the identifiers are resolved.
- * The resolving process will take each identifier, look it up using the {@code ReferenceData},
- * and return a new "resolved" instance.
- * Typically the result is of a type that is optimized for pricing.
+ * 当调用{@code resolve(ReferenceData)}方法时，将解析标识符。
+ * 解析过程将接受每个identifier（标识符），使用{@code ReferenceData}查找它，并返回一个新的“已解析”实例。
+ * 通常，结果是为定价优化的类型。
  * <p>
- * Resolved objects may be bound to data that changes over time, such as holiday calendars.
- * If the data changes, such as the addition of a new holiday, the resolved form will not be updated.
- * Care must be taken when placing the resolved form in a cache or persistence layer.
+ * 已解析的对象可能绑定到随时间变化的数据，比如holiday calendars。
+ * 如果数据发生更改，例如添加了新的假期，则不会更新已解析的表单。
+ * 在将解析后的表单放入缓存或持久层时必须小心。
  * <p>
- * Implementations must be immutable and thread-safe beans.
- * 
- * @param <T>  the type of the resolved result
+ * 实现必须是不可变的、线程安全的bean。
+ *
+ * @param <T> the type of the resolved result
  */
 public interface Resolvable<T> {
 
-  /**
-   * Resolves this object using the specified reference data.
-   * <p>
-   * This converts the object implementing this interface to the equivalent resolved form.
-   * All {@link ReferenceDataId} identifiers in this instance will be resolved.
-   * The resolved form will typically be a type that is optimized for pricing.
-   * <p>
-   * Resolved objects may be bound to data that changes over time, such as holiday calendars.
-   * If the data changes, such as the addition of a new holiday, the resolved form will not be updated.
-   * Care must be taken when placing the resolved form in a cache or persistence layer.
-   * 
-   * @param refData  the reference data to use when resolving
-   * @return the resolved instance
-   * @throws ReferenceDataNotFoundException if an identifier cannot be resolved in the reference data
-   * @throws RuntimeException if unable to resolve due to an invalid definition
-   */
-  public abstract T resolve(ReferenceData refData);
+    /**
+     * 使用指定的ReferenceData解析此对象。
+     * <p>
+     * 将实现此接口的对象转换为等效的已解析表单。
+     * 将解析此实例中的所有{@link ReferenceDataId}标识符。解析后的表单通常是针对定价进行优化的类型。
+     * <p>
+     * 已解析的对象可能绑定到随时间变化的数据，比如holiday calendars。
+     * 如果数据发生更改，例如添加了新的holiday，则不会更新已解析的表单。在将解析后的表单放入缓存或持久层时必须小心。
+     *
+     * @param refData the reference data to use when resolving
+     * @return the resolved instance
+     * @throws ReferenceDataNotFoundException if an identifier cannot be resolved in the reference data
+     * @throws RuntimeException               if unable to resolve due to an invalid definition
+     */
+    public abstract T resolve(ReferenceData refData);
 
 }
