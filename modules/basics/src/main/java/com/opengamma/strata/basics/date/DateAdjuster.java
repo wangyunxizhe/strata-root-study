c594bf97adcee6facf1086aa9c075c0721d74b90
wangyunxizhe <543456229@qq.com>
2019-06-12 14:50:56 +0800

    功能界面，可以调整日期

--- modules/basics/src/main/java/com/opengamma/strata/basics/date/DateAdjuster.java
+++ modules/basics/src/main/java/com/opengamma/strata/basics/date/DateAdjuster.java
@@ -11,43 +11,41 @@ import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
 /**
- * Functional interface that can adjust a date.
+ * 功能界面，可以调整日期。
  * <p>
- * This extends {@link TemporalAdjuster} for those cases where the temporal to
- * be adjusted is an ISO-8601 date.
+ * 对于需要调整的时间是ISO-8601日期的情况，这扩展了{@link TemporalAdjuster}。
  */
 @FunctionalInterface
 public interface DateAdjuster
-    extends TemporalAdjuster {
+        extends TemporalAdjuster {
 
-  /**
-   * Adjusts the date according to the rules of the implementation.
-   * <p>
-   * Implementations must specify how the date is adjusted.
-   * 
-   * @param date  the date to adjust
-   * @return the adjusted date
-   * @throws DateTimeException if unable to make the adjustment
-   * @throws ArithmeticException if numeric overflow occurs
-   */
-  public abstract LocalDate adjust(LocalDate date);
+    /**
+     * 根据实施细则调整日期。
+     * <p>
+     * 实现必须指定如何调整日期。
+     *
+     * @param date 调整前的日期
+     * @return 调整后的日期
+     * @throws DateTimeException   if unable to make the adjustment
+     * @throws ArithmeticException if numeric overflow occurs
+     */
+    public abstract LocalDate adjust(LocalDate date);
 
-  /**
-   * Adjusts the temporal according to the rules of the implementation.
-   * <p>
-   * This method implements {@link TemporalAdjuster} by calling {@link #adjust(LocalDate)}.
-   * Note that conversion to {@code LocalDate} ignores the calendar system
-   * of the input, which is the desired behaviour in this case.
-   * 
-   * @param temporal  the temporal to adjust
-   * @return the adjusted temporal
-   * @throws DateTimeException if unable to make the adjustment
-   * @throws ArithmeticException if numeric overflow occurs
-   */
-  @Override
-  public default Temporal adjustInto(Temporal temporal) {
-    // conversion to LocalDate ensures that other calendar systems are ignored
-    return temporal.with(adjust(LocalDate.from(temporal)));
-  }
+    /**
+     * 根据实施规则调整时间。
+     * <p>
+     * 这个方法通过调用{@link #adjust(LocalDate)}来实现{@link TemporalAdjuster}。
+     * 注意，转换到{@code LocalDate}会忽略输入的日历系统，这是本例中需要的行为。
+     *
+     * @param temporal the temporal to adjust
+     * @return the adjusted temporal
+     * @throws DateTimeException   if unable to make the adjustment
+     * @throws ArithmeticException if numeric overflow occurs
+     */
+    @Override
+    public default Temporal adjustInto(Temporal temporal) {
+        // conversion to LocalDate ensures that other calendar systems are ignored
+        return temporal.with(adjust(LocalDate.from(temporal)));
+    }
 
 }
